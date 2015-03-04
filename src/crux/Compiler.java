1ffa528ad8bf1e4157e1e11ab8aff63063c61125
allansbeckman <allansbeckman@gmail.com>
2015-03-04 10:52:03 -0800

    Added Project 6 Code

--- src/crux/Compiler.java
+++ src/crux/Compiler.java
@@ -9,22 +9,10 @@ public class Compiler {
     public static String studentID = "21588725";
     public static String uciNetID = "beckmana";
     
-    public static boolean release = false;
-    
     public static void main(String[] args)
     {
-    	if (release) {
-    		releasedMain(args[0]);
-    	} else {
-    		for (int i=1; i<=15; ++i) {
-		    	String sourceFilename = String.format("tests/test%02d.crx", i);
-	        	fileCompile(sourceFilename);
-    		}
-    	}
-    }
-    
-    public static void releasedMain(String sourceFilename)
-    {
+        String sourceFilename = args[0];
+        
         Scanner s = null;
         try {
             s = new Scanner(new FileReader(sourceFilename));
@@ -37,7 +25,7 @@ public class Compiler {
         Parser p = new Parser(s);
         ast.Command syntaxTree = p.parse();
         if (p.hasError()) {
-            System.out.println("Error parsing file.");
+            System.out.println("Error parsing file " + sourceFilename);
             System.out.println(p.errorReport());
             System.exit(-3);
         }
@@ -45,54 +33,31 @@ public class Compiler {
         types.TypeChecker tc = new types.TypeChecker();
         tc.check(syntaxTree);
         if (tc.hasError()) {
-            System.out.println("Error type-checking file.");
+            System.out.println("Error type-checking file " + sourceFilename);
             System.out.println(tc.errorReport());
             System.exit(-4);
         }
-        System.out.println("Crux Program has no type errors.");
-    }
-
-    public static void fileCompile(String sourceFilename)
-    {
-        String outFilename = sourceFilename.replace(".crx", ".out");
-		System.out.println("Lab5.implementation outputing for " + outFilename);
-		
-		PrintStream outStream = null;
-		try {
-	        File outFile = new File(outFilename);
-	        outStream = new PrintStream(outFile);
-		} catch (IOException e) {
-	        e.printStackTrace();
-            System.err.println("Error creating output file: \"" + outFilename + "\"");
-		}
-    	
-    	Scanner s = null;
-        try {
-            s = new Scanner(new FileReader(sourceFilename));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Error accessing the source file: \"" + sourceFilename + "\"");
-            System.exit(-2);
-        }
-
-        Parser p = new Parser(s);
-        ast.Command syntaxTree = p.parse();
-        if (p.hasError()) {
-            System.out.println("Error parsing file.");
-            System.out.println(p.errorReport());
-            System.exit(-3);
-        }
-            
-        types.TypeChecker tc = new types.TypeChecker();
-        tc.check(syntaxTree);
-        if (tc.hasError()) {
-            outStream.println("Error type-checking file.");
-            outStream.println(tc.errorReport());
-            outStream.close();
-        } else {
-	        outStream.println("Crux Program has no type errors.");
+        
+        mips.CodeGen cg = new mips.CodeGen(tc);
+        cg.generate(syntaxTree);
+        if (cg.hasError()) {
+            System.out.println("Error generating code for file " + sourceFilename);
+            System.out.println(cg.errorReport());
+            System.exit(-5);
         }
         
+        String asmFilename = sourceFilename.replace(".crx", ".asm");
+        try {
+            mips.Program prog = cg.getProgram();
+            File asmFile = new File(asmFilename);
+            PrintStream ps = new PrintStream(asmFile);
+            prog.print(ps);
+            ps.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error writing assembly file: \"" + asmFilename + "\"");
+            System.exit(-6);
+        }
     }
 }
     
